175a02851bf081ee1d6fedabe9689052597b6a71
NeonWilderness <neonwilderness@ymail.com>
2015-03-20 10:17:06 +0100

    Amend dir structures, added gulpfile procedures

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,25 @@
 var browserify = require('browserify');
+var concatcss = require('gulp-concat-css');
+var footer = require('gulp-footer');
 var gulp = require('gulp');
-var transform = require('vinyl-transform');
 var jade = require('gulp-jade');
-var uglify = require('gulp-uglify');
-var sourcemaps = require('gulp-sourcemaps');
 var rename = require('gulp-rename');
+var sourcemaps = require('gulp-sourcemaps');
+var transform = require('vinyl-transform');
+var trim = require('gulp-trim');
+var uglify = require('gulp-uglify');
 
 gulp.task( 'default', ['jade2skin'], function(){
+});
+
+gulp.task( 'bundlecss', function(){
+
+    gulp.src('src/css/*.css')
+        .pipe(rename(function(path){ // e.g. "foundation.css"
+            path.basename = "css"+path.basename.substr(0,1).toUpperCase()+path.basename.substr(1); // cssFoundation
+            path.extname = ".skin";
+        }))
+        .pipe(gulp.dest('dist/skins/Site'));
 });
 
 gulp.task( 'headjs', function(){
@@ -40,17 +53,20 @@ gulp.task( 'bodyjs', function(){
 });
 
 gulp.task( 'jade2skin', function(){
+
     gulp.src('src/skins/*.jade')
         .pipe(jade({pretty:true})) // Site-page.jade
+        .pipe(trim()) // trim any whitespace
         .pipe(rename(function(path){
             var parts = path.basename.split("-"); // Site-page.html
             path.dirname += "/"+parts[0]; // Site
             path.basename = parts[1]; // page
             path.extname = ".skin";
         }))
-        .pipe(gulp.dest("dist/skins"));
+        .pipe(gulp.dest('dist/skins'));
+
     gulp.src('src/preferences.jade')
         .pipe(jade({pretty:true}))
-        .pipe(rename("preferences.xml"))
-        .pipe(gulp.dest("dist"));
+        .pipe(rename('preferences.xml'))
+        .pipe(gulp.dest('dist'));
 });
