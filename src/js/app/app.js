da48ac2c04c6ae3a4d34411d074b293ea79e4dc3
NeonWilderness <neonwilderness@ymail.com>
2015-03-27 04:18:24 +0100

    Special MenuItems and Toolbar updates

--- src/js/app/app.js
+++ src/js/app/app.js
@@ -1,26 +1,37 @@
 var angular = require('angular'),
-    utils = require('./utils.js'),
-    jq = angular.element;
+    utils = require('./utils.js');
 
 //--- Pimp old Twoday classes, e.g. button in a .message
 utils.pimpClasses();
 
 //--- Set up the Angular controller
 var f5SkinApp = angular.module('f5SkinApp', []);
-f5SkinApp.controller('F5SkinController', ['$scope', function ($scope){
-    var stdPreferences = JSON.parse(jq('#stdPreferences').text() || "{}"),
-        usrPreferences = JSON.parse(jq("#usrPreferences").text() || "{}");
-    $scope.param = angular.extend( {}, stdPreferences, usrPreferences, f5CoreInfo );
+f5SkinApp.controller('F5SkinController', ['$scope', '$sce', function ($scope, $sce){
+    var stdPreferences = JSON.parse($('#stdPreferences').text() || "{}"),
+        usrPreferences = JSON.parse($("#usrPreferences").text() || "{}");
+    $scope.param = $.extend( {}, stdPreferences, usrPreferences, f5CoreInfo );
     if ($scope.param.topbar.menuItems.topics.text==="")
-        $scope.param.topbar.menuItems.topics.text = $scope.siteAlias.toLowerCase();
-    $scope.param.topbar.menuItems.topics.class["has-dropdown"] = jq('#toolbarTopics').has('.dropdown');
-    $scope.param.topbar.breadcrumbs = utils.getBreadCrumbs(this.param.navPath);
-    $scope.param.isLoggedIn = function(){ return (this.param.userName.length>0); };
+        $scope.param.topbar.menuItems.topics.text = $scope.param.siteAlias.toLowerCase();
+    $scope.param.topbar.menuItems.topics.class["has-dropdown"] = $('#toolbarTopics').has('.dropdown');
+    $scope.param.topbar.menuItems.breadcrumbs.items = utils.getBreadcrumbs();
+    $scope.param.topbar.menuItems.abo.items = $sce.trustAsHtml(utils.getSpecialMenu("#aboMenu"));
+    $scope.param.topbar.menuItems.images.items = $sce.trustAsHtml(utils.getSpecialMenu("#imageMenu"));
+    $scope.param.isLoggedIn = function(){ return ($scope.param.userName.length>0); };
     $scope.param.isAdmin = function(){
-        return (this.param.isLoggedIn() && this.param.userRole()==="Administrator");
+        return ($scope.param.isLoggedIn() && $scope.param.userRole()==="Administrator");
     };
     $scope.param.isContributor = function(){
-        return (this.param.isLoggedIn() && (this.param.userRole()==="Contributor" || this.param.userRole()==="Administrator"));
+        return ($scope.param.isLoggedIn() && ($scope.param.userRole()==="Contributor" || $scope.param.userRole()==="Administrator"));
     };
-    $scope.param.sendMail = function(){ window.location.href = utils.rot13(this.param.topbar.mailIcon.href); }
+    $scope.param.sendMail = function(){
+        window.location.href = utils.rot13($scope.param.topbar.mailIcon.href);
+    };
 }]);
+
+//------- Defines a new directive for generating a type-dependant user custom menu-item (see param.topbar.addCustomMenu.items)
+f5SkinApp.directive('custom-menuitem', function(menuitem){
+    return {
+        restrict: 'E', // E = element, A = attribute, C = class, M = comment
+        template: '<li>'+(menuitem.type==='label' ? '<label>'+menuitem.text+'</label>' : '<a target="_blank" href="'+menuitem.href+'">'+menuitem.text+'</a>')+'</li>'
+    };
+});
--- src/js/app/utils.js
+++ src/js/app/utils.js
@@ -1,17 +1,26 @@
-var jq = require('angular').element;
 module.exports = {
+
 //- Extract breadcrumbs out of Twoday's modToolbarNavigationpath-macro
-    getBreadCrumbs: function(navPath){
-        return (jq(navPath).find(":not(a[id])").wrap("<li></li>")[0].outerHTML);
+    getBreadcrumbs: function(){
+        var breadcrumbs = $("#breadcrumbsMenu").find(":not(a[id])");
+        return (breadcrumbs.length>0 ? breadcrumbs.wrap("<li></li>")[0].outerHTML : "");
     },
+
+//- Get special menu items (imageMenu/aboMenu) and convert DIVs to LIs
+    getSpecialMenu: function(menuID){
+        return ($(menuID).html().replace(/div>/g, "li>"));
+    },
+
 //- Pimp server generated Twoday classes to enforce Foundation style
     pimpClasses: function(){
-        jq('.message .button').addClass('secondary tiny radius');
+        $(".message .button").addClass("secondary tiny radius");
     },
-//- Rotation13 method to obfuscate email-address
+
+//- Rotation13 method used to obfuscate email-address
     rot13: function(){
         return this.replace(/[a-zA-Z]/g, function(c){
             return String.fromCharCode((c <= "Z" ? 90 : 122) >= (c = c.charCodeAt(0) + 13) ? c : c - 26);
         });
     }
+
 };
